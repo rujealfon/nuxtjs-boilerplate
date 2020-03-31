8407c6672cf876629314de45a82458f8909b54e5
Ruje Alfon <ruje.alfon@bcstechnology.com.au>
2020-03-31 10:06:51 +0800

    update deploy script

--- deploy.sample.sh
+++ deploy.sample.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 # https://nuxtjs.org/faq/deployment-aws-s3-cloudfront/
 
-export AWS_ACCESS_KEY_ID="key"
-export AWS_SECRET_ACCESS_KEY="secret"
-export AWS_BUCKET_NAME="bucket"
-export AWS_CLOUDFRONT="UPPERCASE"
+# make file executable
+set -e
+
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+
+instructions ()
+{
+  echo "*********************************************"
+  echo "Run this script as an npm task              *"
+  echo "$ npm run deploy <env>                      *"
+  echo "env: eg. dev, qa, uat                       *"
+  echo "for example: $ npm run deploy dev           *"
+  echo "*********************************************"
+}
+
+if [ $# -eq 0 ]; then
+  instructions
+  exit 1
+elif [ "$1" == dev ]; then
+  export AWS_BUCKET_NAME=""
+  export AWS_CLOUDFRONT=""
+elif [ "$1" == qa ]; then
+  export AWS_BUCKET_NAME=""
+  export AWS_CLOUDFRONT=""
+elif [ "$1" == uat ]; then
+  export AWS_BUCKET_NAME=""
+  export AWS_CLOUDFRONT=""
+else
+  instructions
+  exit 1
+fi
 
 # Load nvm (node version manager), install node (version in .nvmrc), and npm install packages
 [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh" && nvm use
 # Npm install if not already.
 [ ! -d "node_modules" ] && npm install
 
-npm run generate
+npm run build
 gulp deploy
